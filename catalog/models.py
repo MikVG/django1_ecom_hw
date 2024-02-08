7dc994f4ce0d271ec2df46c8d5a67574fe1db557
Mikhail Glushkov <miggl@mail.ru>
2024-02-08 18:17:04 +0300

    20.1 done task 1, 2

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    """
+    класс для описания модели Product
+    """
+    pass
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+
+class Category(models.Model):
+    """
+    класс для описания модели Category
+    """
+    pass
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
