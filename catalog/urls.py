9dedabf6082dcfe1a188e4ac2921473861bd26a1
Mikhail Glushkov <miggl@mail.ru>
2024-02-28 18:14:00 +0300

    21.1 change FBV on CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, product_detail
+from catalog.views import ProductListView, ProductDetailView, ContactPageView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('contacts/', contacts, name='contacts'),
-    path('catalog/<int:pk>', product_detail, name='product')
+    path('', ProductListView.as_view(), name='home'),
+    path('contacts/', ContactPageView.as_view(), name='contacts'),
+    path('catalog/<int:pk>', ProductDetailView.as_view(), name='product')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,37 +1,15 @@
-from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, TemplateView
 
 from catalog.models import Product
 
 
-def home(request):
-    """
-    функция выдает страницу со всеми продуктами
-    """
-    product_list = Product.objects.all()
-    context = {
-        'object_list': product_list
-    }
-    return render(request, 'catalog/home.html',  context)
+class ProductListView(ListView):
+    model = Product
 
 
-def product_detail(request, pk):
-    """
-    функция выдает страницу с одним продуктом
-    """
-    product = get_object_or_404(Product, pk=pk)
-    context = {
-        'product': product
-    }
-    return render(request, 'catalog/product.html',  context)
+class ProductDetailView(DetailView):
+    model = Product
 
 
-def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(f'name: {name}\n'
-              f'phone: {phone}\n'
-              f'message: {message}')
-
-    return render(request, 'catalog/contacts.html')
+class ContactPageView(TemplateView):
+    template_name = 'catalog/contacts.html'
