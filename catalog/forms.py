082c67d35b26e9a9889f5d99bbcc7255f3369911
Mikhail Glushkov <miggl@mail.ru>
2024-03-22 17:41:14 +0300

    22.1 task 1 done

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.forms import ModelForm
 
 from catalog.models import Product
@@ -8,3 +9,17 @@ class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = ('name', 'description', 'image', 'category', 'price')
+
+    forbidden_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+
+    def clean_name(self):
+        create_data = self.cleaned_data['name']
+        if create_data in self.forbidden_words:
+            raise ValidationError('Нельзя использовать запрещенное слово в названии')
+        return create_data
+
+    def clean_description(self):
+        create_data = self.cleaned_data['description']
+        if create_data in self.forbidden_words:
+            raise ValidationError('Нельзя использовать запрещенное слово в описании')
+        return create_data
