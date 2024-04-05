96025c7405e6c8a62e9b29c4b42ac21cf4318017
Mikhail Glushkov <miggl@mail.ru>
2024-04-05 21:51:47 +0300

    23.2 Task 4 done

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
+
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e+3_bar1h*r-6hm=o$cctf)5$)qfufk=y3zb%byu%oig%@-u-b'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,9 +87,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ecomhw',
-        'USER': 'postgres',
-        'PASSWORD': '12345'
+        'NAME': os.getenv('POSTGRE_DB'),
+        'USER': os.getenv('POSTGRE_USER'),
+        'PASSWORD': os.getenv('POSTGRE_PASSWORD')
     }
 }
 
@@ -139,8 +145,8 @@ LOGOUT_REDIRECT_URL = '/'
 
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
-EMAIL_HOST_USER = "appptn@yandex.ru"
-EMAIL_HOST_PASSWORD = "tmqjykgnmcdkpqef"
+EMAIL_HOST_USER = os.getenv('EMAIL')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 
